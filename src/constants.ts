6f46e4ed5dd61613a36e3d43172722917c4d385b
Eddy <edkosyan2000@gmail.com>
2024-04-30 15:29:15 +0400

    fix bug

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,5 @@
+export const FIRST_FIELD_SIZE = 19;
+export const SELECTED_IN_FIRST_FIELD = 8;
 
-
-
-
-export const FIRST_FIELD_SIZE = 19
-export const SELECTED_IN_FIRST_FIELD = 8
-
-export const SECOND_FIELD_SIZE = 2
-export const SELECTED_IN_SECOND_FIELD = 1
-
-             
+export const SECOND_FIELD_SIZE = 2;
+export const SELECTED_IN_SECOND_FIELD = 1;
